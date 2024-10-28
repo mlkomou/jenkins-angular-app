0d951cd3ead403d9238cdad6d90dd9033bfe2100
Mohamed Lamine KOMOU <assetou@mediasoftci.com>
2024-10-28 17:01:46 +0000

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jenkins-angular-app-test';
+  title = 'jenkins-angular-app-test deploy';
 }
